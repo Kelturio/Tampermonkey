ecb8a38d64e578da9a380a636aa35e236d34a53d
Searinox <searinox@gmx.de>
2018-09-25 01:37:21 +0200

    akk

--- Steamgifts Giveaways - New.user.js
+++ Steamgifts Giveaways - New.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steamgifts Giveaways - New
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.5
 // @description  try to take over the world!
 // @author       Searinox
 // @match        https://www.steamgifts.com/giveaways/new
@@ -13,16 +13,16 @@
     let startTime = 'Sep 25, 2018 4:20 am';
     let endTime = 'Sep 30, 2018 4:20 pm';
     let contributorLevel = '10';
-    let descriptionTxt = [
+    let text = [
         'Akk!',
         'Steam Group: [Searinox Army](https://steamcommunity.com/groups/searinoxarmy#members)'
     ];
-    let descriptionImg = [
+    let images = [
         'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/21/21c3f7cece613ea20a41ac1582b92033f8abb267_full.jpg',
         'https://i.imgur.com/RogoP2g.gif',
         'https://media1.tenor.com/images/b35a2dd687b91f7c10e64a049948822e/tenor.gif'
-    ].map(e => `![Searinox Army](${e})`);
-    let description = [...descriptionTxt, ...descriptionImg].join('\n');
+    ];
+    const header = 'https://steamcdn-a.akamaihd.net/steam/apps/{id}/header.jpg';
     $('form input[name=type]')[0].value = 'key';
     $('form div[data-checkbox-value=key]').addClass('is-selected');
     $('form div.form__row--giveaway-keys.is-hidden').removeClass('is-hidden');
@@ -35,5 +35,18 @@
     $('form input[name=contributor_level]')[0].value = contributorLevel;
     $('form div.ui-slider-range.ui-slider-range-min').css('width', `${contributorLevel * 10}%`);
     $('form span.ui-slider-handle.ui-state-default').css('left', `${contributorLevel * 10}%`);
-    $('form textarea[name=description]')[0].value = description;
+    $('form div.js__autocomplete-data').on('DOMSubtreeModified', function(event) {
+        if (this.children[0] && this.children[0].children[0] && this.children[0].children[0].children) {
+            [...this.children[0].children[0].children].map((e) => {
+                $(e).click(() => {
+                    let img = header.replace('{id}', $(e).find('a')[0].href.replace(/\D+/g, ''));
+                    if(!images.includes(img)) {
+                        images.unshift(img);
+                    }
+                    let description = [...text, ...images.map(e => `![Searinox Army](${e})`)].join('\n');
+                    $('form textarea[name=description]')[0].value = description;
+                });
+            });
+        }
+    });
 })();
