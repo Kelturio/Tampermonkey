59ded332d4e3e5b72f9ec0978192feba0177ea51
Searinox <searinox@gmx.de>
2018-10-09 01:03:32 +0200

    akk sifte

--- sifte.au3_google.user.js
+++ sifte.au3_google.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sifte.au3_google
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.5
 // @description  try to take over the world!
 // @author       Searinox
 // @icon		 https://avatars0.githubusercontent.com/u/16297928?s=460&v=4
@@ -13,15 +13,18 @@
 
 (function() {
     'use strict';
-    let burp = new Audio('https://raw.githubusercontent.com/Kelturio/Misc/master/GTA2/SIFTE/SFX_BURP.wav');
-    let fart = new Audio('https://raw.githubusercontent.com/Kelturio/Misc/master/GTA2/SIFTE/SFX_FART.wav');
+    let url = 'https://raw.githubusercontent.com/Kelturio/Misc/master/GTA2/SIFTE/';
+    let sfx = [
+        'SFX_BURP',
+        'SFX_FART'
+    ].map((e) => new Audio(url.concat(e, '.wav')));
     let play = (audio) => {
         audio.pause();
         audio.currentTime = 0;
         //audio.playbackRate = 0.5;
         audio.play();
     }
-    let playRandom = () => Math.random() < 0.5 ? play(burp) : play(fart);
+    let playRandom = () => Math.random() < 0.5 ? play(sfx[0]) : play(sfx[1]);
     document.onkeydown = playRandom;
     document.addEventListener('click', playRandom);
 })();
