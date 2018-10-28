e76e682e131a659aefd4c3ba143823e835adbf8c
Searinox <searinox@gmx.de>
2018-10-28 22:55:20 +0100

    +

--- sifte.au3_google.user.js
+++ sifte.au3_google.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sifte.au3_google
 // @namespace    http://tampermonkey.net/
-// @version      1.6
+// @version      1.7
 // @description  try to take over the world!
 // @author       Searinox
 // @icon         https://avatars0.githubusercontent.com/u/16297928?s=460&v=4
@@ -11,20 +11,29 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+/* eslint id-length: ["error", { "min": 2 }] */
+/* eslint lines-around-directive: 0 */
+/* eslint max-len: ["error", { "ignoreUrls": true }] */
+/* eslint multiline-comment-style: 0 */
+/* eslint newline-after-var: 0 */
+/* eslint padded-blocks: 0 */
+/* eslint quotes: [2, "single"] */
+/* eslint sort-vars: 0 */
+/* eslint-env browser, es6, greasemonkey */
+
+(function sifte () {
     'use strict';
-    let url = 'https://raw.githubusercontent.com/Kelturio/Misc/master/GTA2/SIFTE/';
-    let sfx = [
-        'SFX_BURP',
-        'SFX_FART'
-    ].map((e) => new Audio(url.concat(e, '.wav')));
-    let play = (audio) => {
-        audio.pause();
-        audio.currentTime = 0;
-        //audio.playbackRate = 0.5;
-        audio.play();
-    }
-    let playRandom = () => play(sfx[Math.floor(Math.random() * sfx.length)]);
+    const url = 'https://raw.githubusercontent.com/Kelturio/Misc/master/GTA2/SIFTE/',
+        sfx = [
+            'SFX_BURP',
+            'SFX_FART'
+        ].map((cv) => new Audio(`${url}${cv}.wav`)),
+        play = (audio) => {
+            audio.pause();
+            audio.currentTime = 0;
+            audio.play();
+        },
+        playRandom = () => play(sfx[Math.floor(Math.random() * sfx.length)]);
     document.onkeydown = playRandom;
     document.addEventListener('click', playRandom);
-})();
+}());
