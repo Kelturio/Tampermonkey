d20e80a9e322e28193a54aa02dbf363fea2d8e52
Searinox <searinox@gmx.de>
2018-11-05 23:38:05 +0100

    -

--- akk.js
+++
@@ -1,9 +0,0 @@
-module staff{
-    // specify (public) exports that can be consumed by
-    // other modules
-    export var baker = {
-        bake: function( item ){
-            console.log('Woo! I just baked ' + item);
-        }
-    }   
-}
