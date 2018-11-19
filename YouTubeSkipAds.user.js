0f667f9dc86722855eb2b08ad803480f9e4b00f1
Searinox <searinox@gmx.de>
2018-11-19 21:50:11 +0100

    +

--- YouTubeSkipAds.user.js
+++ YouTubeSkipAds.user.js
@@ -1,29 +1,36 @@
 // ==UserScript==
 // @name         YouTubeSkipAds
 // @namespace    http://tampermonkey.net/
-// @version      1.3.4
-// @description  try to take over the world!
+// @version      1.3.6
+// @description  Ads removal
+// @description  Enhancer for YouTube™ extension is needed
+// @description  https://www.mrfdev.com/enhancer-for-youtube
+// @description  https://addons.mozilla.org/addon/enhancer-for-youtube/?src=external-mrfdev
+// @description  https://chrome.google.com/webstore/detail/enhancer-for-youtube/ponfpcnoihfmfllpaingbgckeeldkhle
+// @description  https://addons.opera.com/en/extensions/details/enhancer-for-youtube/?display=en-US
+// @description  https://www.microsoft.com/en-us/store/p/enhancer-for-youtube-for-microsoft-edge/9n4f8m7plt38
 // @author       Searinox
 // @icon         https://avatars0.githubusercontent.com/u/16297928?s=460&v=4
+// @updateURL    https://kelturio.github.io/Tampermonkey/YouTubeSkipAds.user.js
 // @match        https://www.youtube.com/watch*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function iife () {
   'use strict'
-  const cssVideo = '#movie_player > div.html5-video-container > video'
-  const cssEnhancerClean = '#enhancer-for-youtube-toolbar > ul > li[data-name=clean]'
-  let lastSrc = ''
-  let i = 0
-  function clean () {
-    return document.querySelector(cssEnhancerClean).click()
+  const css = {
+    enhancerClean: '#enhancer-for-youtube-toolbar > ul > li[data-name=clean]',
+    video        : '#movie_player > div.html5-video-container > video',
   }
-  function skipAd () {
-    let video = document.querySelector(cssVideo)
+  const clean = () => document.querySelector(css.enhancerClean).click()
+  const autoSkip = () => {
+    const video = document.querySelector(css.video)
     if (video && video.src) {
-      (i++ % 8 || video.src !== lastSrc) && clean()
+      (!(i++ % 6) || video.src !== lastSrc) && clean()
       lastSrc = video.src
     }
   }
-  setInterval(skipAd, 420)
-})();
+  let i = 0
+    , lastSrc = ''
+  setInterval(autoSkip, 420)
+}())
